fe05af651925ab2e423d978dc40df56c8591c79d
KritikaE <kritika.one@gmail.com>
2026-05-04 23:41:07 +0530

    Refactor SJF scheduling algorithm for clarity

--- sjf.c
+++ sjf.c
@@ -1,42 +1,51 @@
-//NON PRE-EMPTIVE
+//NON PREMPTIVE
 #include<stdio.h>
 
 void main(){
     int n;
     int at[10], bt[10], ct[10], tat[10], wt[10];
-    
-    printf("Enter the number of process\n");
-    scanf("%d\n",&n);
-    
-    for(int i = 0; i<n; i++){
-        printf("Enter the AT and BT for %d",i);
-        scanf("%d %d",&at[i], &bt[i]);
+    int done[10] = {0};
 
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++){
+        printf("Enter AT and BT for P%d: ", i+1);
+        scanf("%d %d", &at[i], &bt[i]);
     }
-    
-    ct[0]=at[0]+bt[0];
-    
-    int temp; 
-    //sort bt in ascending order
-    for(int i =1; i<n-1; i++){
-        for(int j = i+1; j<n; j++){
-            temp = bt[i]; bt[i] = bt[j]; bt[j] = temp;
-            temp = at[i]; at[i] = at[j]; at[j] = temp;
+
+    int time = 0;
+
+    for(int count = 0; count < n; count++){
+        int min = 9999, idx = -1;
+
+        // find shortest available process
+        for(int i=0; i<n; i++){
+            if(done[i]==0 && at[i] <= time && bt[i] < min){
+                min = bt[i];
+                idx = i;
+            }
+        }
+
+        if(idx == -1){
+            time++;      // no process yet → wait
+            count--;     // retry same iteration
+        }
+        else{
+            time += bt[idx];
+            ct[idx] = time;
+            done[idx] = 1;
         }
     }
-    
-    for(int i=1; i<n; i++){
-            ct[i]=ct[i-1]+bt[i];
+
+    for(int i=0; i<n; i++){
+        tat[i] = ct[i] - at[i];
+        wt[i] = tat[i] - bt[i];
     }
-    
-    for(int i = 0; i<n; i++){
-        tat[i]=ct[i]-at[i];
-        wt[i]=tat[i]-bt[i];
-    }
-    
-    
+
     printf("\nP\tAT\tBT\tCT\tTAT\tWT\n");
-    for(int i =0; i<n; i++){
-        printf("%d\t%d\t%d\t%d\t%d\t%d\n",i,at[i],bt[i],ct[i],tat[i],wt[i]);
+    for(int i=0; i<n; i++){
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n",
+               i+1, at[i], bt[i], ct[i], tat[i], wt[i]);
     }
 }
